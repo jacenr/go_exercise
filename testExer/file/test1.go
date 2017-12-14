d539b198f7cf5246e60ee83fcc8c5cfc66a2aab7
rzcTree <rluckzflyc@gmail.com>
2017-12-14 15:49:05 +0800

    20171214

--- testExer/file/test1.go
+++ testExer/file/test1.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	// "time"
 )
 
 func main() {
-	file := os.Args[1]
-	fi, _ := os.Lstat(file)
-	fmt.Println(fi.Name())
-	fmt.Println(fi.Size())
-	fmt.Println(fi.ModTime())
-	fmt.Println(fi.Mode())
-	fmt.Println(fi.IsDir())
-	fmt.Println(fi.Sys())
+	// file := os.Args[1]
+	// fi, _ := os.Lstat(file)
+	// fmt.Println(fi.Name())
+	// fmt.Println(fi.Size())
+	// fmt.Println(fi.ModTime())
+	// fmt.Println(fi.Mode())
+	// fmt.Println(fi.IsDir())
+	// fmt.Println(fi.Sys())
+
+	// file1 := "SrcFile"
+	f, err := os.Open("SrcFile")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// NEWREADER
+	// readerF1 := bufio.NewReader(f)
+
+	// NEWSCANNER
+	strs := []string{}
+	scannerF1 := bufio.NewScanner(f)
+	scannerF1.Split(bufio.ScanLines)
+	for scannerF1.Scan() {
+		strs = append(strs, scannerF1.Text())
+	}
+	fmt.Println(len(strs))
+	for _, txt := range strs {
+		fmt.Printf("%s\n", txt)
+	}
+	fmt.Println(strs[0] == strs[2])
 }
